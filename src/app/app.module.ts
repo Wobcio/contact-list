fe2660d7ac8f9a4ea067079c7a47624542f5d6ed
Igor Batko <igor.batko.batko@gmail.com>
2023-02-19 14:09:42 +0100

    Setting up an envoirment, adding hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroComponent } from './hero/hero.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeroComponent, ContactListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
