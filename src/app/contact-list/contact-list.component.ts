e74957b19b9dd8171082906cb7e5092c35ee0943
Igor Batko <igor.batko.batko@gmail.com>
2023-02-19 15:49:36 +0100

    Add contact list and contact module, add styling to them

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Contact } from './contact.module';
+
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.sass']
+  styleUrls: ['./contact-list.component.sass'],
 })
 export class ContactListComponent {
-
+  contacts: Contact[] = [
+    new Contact('This is a test contact', 'This is a test description'),
+    new Contact('This is a test contact 2', 'This is a test description 2'),
+    new Contact('This is a test contact 3', 'This is a test description 3'),
+  ];
 }
---
+++ src/app/contact-list/contact.module.ts
@@ -0,0 +1,9 @@
+export class Contact {
+  public name: string;
+  public description: string;
+
+  constructor(name: string, desc: string) {
+    this.name = name;
+    this.description = desc;
+  }
+}
